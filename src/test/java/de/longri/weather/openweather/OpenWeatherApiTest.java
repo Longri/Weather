bc5082815533246bd992ce28887f2e9a325dc402
Longri <www.longri.de@googlemail.com>
2015-11-19 22:41:46 +0100

    + add first JUnit test

--- src/test/java/de/longri/weather/openweather/OpenWeatherApiTest.java
+++ src/test/java/de/longri/weather/openweather/OpenWeatherApiTest.java
@@ -1,6 +1,11 @@
 package de.longri.weather.openweather;
 
+import de.longri.serializable.SerializableArrayList;
+import de.longri.weather.Info;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Longri on 19.11.15.
@@ -11,7 +16,19 @@ public class OpenWeatherApiTest {
     public void testWeatherInfo() throws Exception {
 
         // load OpenWeather-API key from git ignored file OPW-Key.txt
+        String key = null;
+        try {
+            key = IOUtils.toString(getClass().getResourceAsStream("/OPW-Key.txt"));
+        } catch (Exception e) {
+            assertTrue("Can't run test without API key! Store the key into \"OPW-Key.txt\"", false);
+        }
 
+        OpenWeatherApi api = new OpenWeatherApi(key);
+        SerializableArrayList<Info> result = api.getCurrentWeatherInfo(13.1, 14.4);
+
+        assertTrue(result != null);
+        assertTrue(result.get(0) != null);
+        System.out.println(key);
 
     }
 }
